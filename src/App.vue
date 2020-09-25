f592964e96d11e590ef42f3b0aeebbeda3cabdbe
HARRiE_Dev <62064385+HARRiE13-Dev@users.noreply.github.com>
2020-09-25 23:17:29 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="https://img.icons8.com/color/48/000000/astronaut.png" height="200px"/>
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <anime/>
+  </div>
+</template>
+
+<script>
+
+import anime from './components/anime.vue'
+
+export default {
+  name: 'App',
+  components: {
+
+    anime
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/anime.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="diffy">
+      <h1>
+        Galaxy View in Your Birth
+      </h1>
+    <input type="text" v-model="name" />
+    <button class="btn btn-warning" @click="searchData()">ENTER</button>
+    <div>
+      <h2>
+        Ex: 1999-10-29
+      </h2>
+      <h3>
+        **note : This is API NASA Your Can see your galaxy in your birthday 
+        Let's get it's on !!! Enjoy in my web. 
+        
+      </h3>
+
+      
+    </div>
+    <b-card class="card bg-indigo">
+      <b-card
+        v-for="data in resultData"
+        :key="data.date"
+        :title="data.title"
+        :img-src="data.hdurl"
+      >
+        <div>{{ data.explanation }}</div>
+       
+      </b-card>
+    </b-card>
+     <h5>Copyright by | HARRiE Dev</h5>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      resultData: null,
+      name: "",
+    };
+  },
+
+  methods: {
+    searchData() {
+      axios
+        .get(
+          "https://api.nasa.gov/planetary/apod?api_key=PjegsGGK7fmIKaCllSdF7lC4svqoD7mK0CTT24KR&date=" +
+            this.name
+        )
+        .then((response) => {
+          this.resultData = response;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
+</script>
+
+<style>
+#diffy {
+  color: black;
+  font-weight: 600;
+  background: blanchedalmond;
+}
+</style>
